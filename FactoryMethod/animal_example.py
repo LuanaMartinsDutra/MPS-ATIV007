9ad1adb35e4983c68e39f6ed6a0ca70f3de211f9
Melinne Diniz Oliveira <mddo.snf21@uea.edu.br>
2023-02-21 12:56:58 -0300

    :sparkles: add factory method

---
+++ FactoryMethod/animal_example.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+# Classe abstrata que define a interface comum para todos os animais que podem ser criados
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+# Classes que implementam a interface Animal
+class Dog(Animal):
+    def make_sound(self):
+        return "au au"
+
+class Cat(Animal):
+    def make_sound(self):
+        return "miau"
+
+# Classe abstrata que contém um factory method abstrato para criar objetos Animal
+class AnimalFactory(ABC):
+    @abstractmethod
+    def create_animal(self) -> Animal:
+        pass
+
+# Classes que estendem a AnimalFactory, cada uma responsável por criar um tipo específico de animal
+class DogFactory(AnimalFactory):
+    def create_animal(self) -> Animal:
+        return Dog()
+
+class CatFactory(AnimalFactory):
+    def create_animal(self) -> Animal:
+        return Cat()
+
+# Cliente que usa a AnimalFactory para criar diferentes tipos de animais
+class AnimalClient:
+    def __init__(self, factory: AnimalFactory):
+        self.factory = factory
+
+    def get_animal_sound(self):
+        animal = self.factory.create_animal()
+        return animal.make_sound()
+
+
+dog_factory = DogFactory()
+client = AnimalClient(dog_factory)
+print(client.get_animal_sound())
+
+cat_factory = CatFactory()
+client = AnimalClient(cat_factory)
+print(client.get_animal_sound())
---
+++ FactoryMethod/database_example.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+class DatabaseConnection(ABC):
+    @abstractmethod
+    def connect(self):
+        pass
+
+class MySQLConnection(DatabaseConnection):
+    def connect(self):
+        print("Conectando ao banco de dados MySQL...")
+
+class PostgreSQLConnection(DatabaseConnection):
+    def connect(self):
+        print("Conectando ao banco de dados PostgreSQL...")
+
+class ConnectionFactory(ABC):
+    @abstractmethod
+    def create_connection(self) -> DatabaseConnection:
+        pass
+
+class MySQLConnectionFactory(ConnectionFactory):
+    def create_connection(self) -> DatabaseConnection:
+        return MySQLConnection()
+
+class PostgreSQLConnectionFactory(ConnectionFactory):
+    def create_connection(self) -> DatabaseConnection:
+        return PostgreSQLConnection()
+
+class DatabaseClient:
+    def __init__(self, factory: ConnectionFactory):
+        self.factory = factory
+
+    def connect_to_database(self):
+        connection = self.factory.create_connection()
+        connection.connect()
+
+
+
+mysql_factory = MySQLConnectionFactory()
+client = DatabaseClient(mysql_factory)
+client.connect_to_database()
+
+postgresql_factory = PostgreSQLConnectionFactory()
+client = DatabaseClient(postgresql_factory)
+client.connect_to_database()
