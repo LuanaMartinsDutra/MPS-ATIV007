a2f1cb6c462a653fa5c7aeb038f314c29e0c5764
LucasCruz <lca.snf21@uea.edu.br>
2023-02-23 10:40:12 -0400

    add class 'originator'

--- Memento/memento.py
+++ Memento/memento.py
@@ -5,3 +5,23 @@ class Memento:
         
     def get_state(self):
         return self._state
+
+class Originator:
+    """
+    Classe Originator cria um Memento contendo um snapshot do seu estado atual e
+    utiliza um Memento para restaurar o seu estado anterior.
+    """
+    def __init__(self, state):
+        self._state = state
+    
+    def set_state(self, state):
+        print(f"Originator: Mudando estado para {state}")
+        self._state = state
+    
+    def save_state(self):
+        print("Originator: Salvando estado em Memento")
+        return Memento(self._state)
+    
+    def restore_state(self, memento):
+        self._state = memento.get_state()
+        print(f"Originator: Estado restaurado para {self._state}")
