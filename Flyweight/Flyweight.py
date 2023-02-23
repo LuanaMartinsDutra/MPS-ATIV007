485b7e939ee5ad1689e0215a5887ab2ee20c5b82
Adriano-Henrique-de-Souza-Andrade <ahdsa.snf21@uea.edu.br>
2023-02-22 22:24:41 -0300

    Padrão Flyweight
    Exemplo de geração de objetos

---
+++ Flyweight/Flyweight.py
@@ -0,0 +1,25 @@
+class Object:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+class ObjectFlyweight:
+    def __init__(self):
+        self.objects = {}
+
+    def get_object(self, name, color):
+        if name not in self.objects:
+            self.objects[name] = Object(name, color)
+        return self.objects[name]
+
+
+# Código de teste
+object_flyweight = ObjectFlyweight()
+
+object1 = object_flyweight.get_object('circle', 'green')
+object2 = object_flyweight.get_object('square', 'blue')
+object3 = object_flyweight.get_object('circle', 'red')
+
+print("Objeto 1:", object1.name, object1.color)
+print("Objeto 2:", object2.name, object2.color)
+print("Objeto 3:", object3.name, object3.color)
