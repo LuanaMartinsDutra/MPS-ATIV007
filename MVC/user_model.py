68a554145c7416b00db819c18aca6501c4b5e853
Tiago Farias Barbosa <tfb.snf21@uea.edu.br>
2023-02-27 13:33:07 -0300

    MVC examples

---
+++ MVC/user_model.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, name: str, email: str):
+        self.name = name
+        self.email = email
+
+
+class UserDAO:
+    def __init__(self):
+        self.users = list()
+    
+    def save_user(self, name: str, email: str) -> None:
+        new_user = User(name=name, email=email)
+        self.users.append(new_user)
+    
+    def get_users(self) -> list[User]:
+        return self.users
