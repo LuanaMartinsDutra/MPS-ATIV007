ddef0eb218fa1687560ec67b564dc42b6a56a123
Adriano Henrique de Souza Andrade <ahdsa.snf21@uea.edu.br>
2023-03-02 17:47:01 -0400

    Update Flyweight.py

--- Flyweight/Flyweight.py
+++ Flyweight/Flyweight.py
@@ -3,47 +3,28 @@ class Object:
         self.name = name
         self.color = color
 
-class NameFlyweight:
-    def __init__(self):
-        self.names = {}
-
-    def get_name(self, name):
-        if name not in self.names:
-            self.names[name] = name
-        return self.names[name]
-
-class ColorFlyweight:
-    def __init__(self):
-        self.colors = {}
-
-    def get_color(self, color):
-        if color not in self.colors:
-            self.colors[color] = color
-        return self.colors[color]
-
 class ObjectFlyweight:
     def __init__(self):
-        self.names = NameFlyweight()
-        self.colors = ColorFlyweight()
         self.objects = {}
 
-    def get_object(self, name, color):
-        name = self.names.get_name(name)
-        color = self.colors.get_color(color)
-        if (name, color) not in self.objects:
-            self.objects[(name, color)] = Object(name, color)
-        return self.objects[(name, color)]
+    def get_object(self, name):
+        if name not in self.objects:
+            self.objects[name] = Object(name, None)
+        return self.objects[name]
 
 
 # Código de teste
 object_flyweight = ObjectFlyweight()
 
-object1 = object_flyweight.get_object('circle', 'green')
-object2 = object_flyweight.get_object('square', 'blue')
-object3 = object_flyweight.get_object('circle', 'red')
-object4 = object_flyweight.get_object('triangle', 'red')
+object1 = object_flyweight.get_object('circle')
+object1.color = 'green'
+
+object2 = object_flyweight.get_object('square')
+object2.color = 'blue'
+
+object3 = object_flyweight.get_object('circle')
+object3.color = 'red'
 
 print("Objeto 1:", object1.name, object1.color)
 print("Objeto 2:", object2.name, object2.color)
 print("Objeto 3:", object3.name, object3.color)
-print("Objeto 4:", object4.name, object4.color)
