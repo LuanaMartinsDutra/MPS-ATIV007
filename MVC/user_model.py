26a9f8176d26f797c7a81815e3ca3fc42c61f189
Níckolas Javier Santos Lívero <njsl.snf21@uea.edu.br>
2023-03-02 17:26:11 -0400

    update comments

--- MVC/user_model.py
+++ MVC/user_model.py
@@ -1,3 +1,7 @@
+# Integrantes:
+# Tiago Farias Barbosa 2115310025
+# Nickolas Javier Santos Livero 2115310042
+
 class User:
     def __init__(self, name: str, email: str):
         self.name = name
